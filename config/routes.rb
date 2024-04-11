b37863b5130374bfbbf6b18adcbe8fc6a9a6ea91
evabanegacom <udegbue69@gmail.com>
2024-04-11 09:53:51 +0100

    worked on product and seller update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static#index'
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[create index destroy]
+      resources :users, only: %i[create index destroy update show]
 
       post '/activate', to: 'users#activate'
       
