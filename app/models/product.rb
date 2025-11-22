62669fbfcef54001b4aea822ce2dd0c5e869ae2a
evabanegacom <udegbue69@gmail.com>
2025-11-22 11:34:20 +0100

    added seller dashboard details

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
   validates :sold_by, presence: true
   validates :contact_number, presence: true
   validates :product_number, presence: true
+  has_many :sales
   mount_uploader :pictureOne, AvatarUploader
   mount_uploader :download_file, DownloadFileUploader
   mount_uploader :pictureTwo, AvatarUploader
